bad12cdeb04cfb3ecc71754135b9698575752959
Ronald-patrick <ronald.patrick1101@gmail.com>
2025-03-09 13:05:01 +0530

    Added security chain

--- src/main/java/com/ronald/springboot/cruddemo/config/SecurityConfig.java
+++ src/main/java/com/ronald/springboot/cruddemo/config/SecurityConfig.java
@@ -2,34 +2,67 @@ package com.ronald.springboot.cruddemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
+
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
 
-        UserDetails ron = User.builder()
-                .username("Ron")
-                .password("{noop}user123")
-                .roles("EMPLOYEE")
-                .build();
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}user123")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//
+//        UserDetails ron = User.builder()
+//                .username("Ron")
+//                .password("{noop}user123")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails susan = User.builder()
+//                .username("susan")
+//                .password("{noop}user123")
+//                .roles("EMPLOYEE","MANAGER")
+//                .build();
+//
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}user123")
+//                .roles("EMPLOYEE","MANAGER","ADMIN")
+//                .build();
+//
+//
+//        return new InMemoryUserDetailsManager(ron,mary,susan);
+//    }
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}user123")
-                .roles("EMPLOYEE","MANAGER","ADMIN")
-                .build();
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
 
+        httpSecurity.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
 
-        return new InMemoryUserDetailsManager(ron,mary,susan);
+        );
+
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        httpSecurity.csrf(csrf -> csrf.disable());
+
+        return httpSecurity.build();
     }
 }
